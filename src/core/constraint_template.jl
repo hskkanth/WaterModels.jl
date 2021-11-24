b7f56bd6f7dd33341890411a4c35be2dda01c83d
Byron Tasseff <tasseff@users.noreply.github.com>
2021-11-24 15:08:09 -0700

    Rename `_NODE_CONNECTING_COMPONENTS` to `_LINK_COMPONENTS`.

--- src/core/constraint_template.jl
+++ src/core/constraint_template.jl
@@ -1,8 +1,8 @@
 # Constraint templates help simplify data wrangling across multiple water network
 # optimization formulations by providing an abstraction layer between the network data and
 # network constraint definitions. Each constraint template's job is to extract the required
-# parameters from a given network data structure and pass the data as named arguments to
-# the water network optimization constraints. Here, constraint templates should always be
+# parameters from a given network data structure and pass the data as named arguments to the
+# water network optimization constraints. Here, constraint templates should always be
 # defined over the type `AbstractWaterModel` and should never refer to model variables.
 
 
